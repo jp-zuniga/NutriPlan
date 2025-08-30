3ddf300321b5fa95ba3bc44237e5e17fcdbe2943
sea2horses <thefuzegamer06@gmail.com>
2025-08-30 17:51:37 -0600

    Diseño inicial del home page

--- frontend/NutriPlan-frontend/src/routes/+layout.svelte
+++ frontend/NutriPlan-frontend/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href="/styles/main.css" />
+	<link rel="stylesheet" href="/styles/home.css" />
 </svelte:head>
 
 {@render children?.()}
--- frontend/NutriPlan-frontend/src/routes/+page.svelte
+++ frontend/NutriPlan-frontend/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="body">
+	<div id="sidebar">
+		<ul>
+			<li>My Account</li>
+			<li>New Recipes</li>
+			<li>Favorites</li>
+			<li>Log out</li>
+		</ul>
+	</div>
+	<div id="main-content">
+		<div class="recipe-card">
+			<div class="recipe-image-container">
+				<img
+					class="recipe-image"
+					src="https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480_1_5x/img/recipe/ras/Assets/162ADE57-4E13-4FA2-9A36-E408323336AE/Derivates/19f90d7d-7a1b-40f4-b912-d9f8bb9d2fbb.jpg"
+					alt="Dedos de Pollo"
+				/>
+				<div class="recipe-image-gradient" />
+			</div>
+			<div class="recipe-info">
+				<div class="recipe-title">Dedos de Pollo</div>
+			</div>
+		</div>
+	</div>
+</div>
