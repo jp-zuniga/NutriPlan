0a1980a9e0311074c02cc750999593d537c744f8
sea2horses <thefuzegamer06@gmail.com>
2025-08-31 17:43:58 -0600

    Unas cuantas paginas y rutas de prueba

---
+++ frontend/NutriPlan-frontend/src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/global.css" />
+</svelte:head>
+
+<h1>Login Page</h1>
+
+<div id="login-form">
+	<span>Username</span>
+	<input type="text" />
+
+	<span>Password</span>
+	<input type="password" />
+</div>
---
+++ frontend/NutriPlan-frontend/src/routes/recipes/[id]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	// Load slug
+	import { page } from '$app/stores';
+	import { obtenerRecetaPorID } from '$lib/classes/receta';
+	import RecipeCard from '$lib/components/RecipeCard.svelte';
+	import { initialRecipes } from '$lib/stores/recipes';
+
+	let receta = obtenerRecetaPorID($initialRecipes, $page.params.id);
+	console.log(receta);
+</script>
+
+<h1>Receta con ID: {$page.params.id}</h1>
+{#if receta}
+	<RecipeCard {receta} />
+{:else}
+	<p>La receta no fue encontrada</p>
+{/if}
