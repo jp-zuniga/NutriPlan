42a3aa4738cc0223971266431f12bcf56b90406c
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-21 14:02:49 -0600

    cositas

--- backend/src/manage.py
+++ backend/src/manage.py
@@ -16,16 +16,14 @@ def main() -> None:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
         msg = (
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         )
-        raise ImportError(
-            msg
-        ) from exc
+        raise ImportError(msg) from exc
 
     execute_from_command_line(sys.argv)
 
