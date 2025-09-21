73b0347215d8294f79619d1c565aa3c03f78ed58
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-21 14:02:49 -0600

    cositas

--- backend/src/backend/wsgi.py
+++ backend/src/backend/wsgi.py
@@ -1,7 +1,5 @@
 """
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
+WSGI config for Django project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
