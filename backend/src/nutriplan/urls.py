be197d672d9ddaa8c8bdb343a983d4edfc389e0a
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-04 15:32:22 -0600

    Crear endpoints para verificacion y refresh de tokens

--- backend/src/nutriplan/urls.py
+++ backend/src/nutriplan/urls.py
@@ -4,6 +4,7 @@ URL and routing configuration.
 
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from nutriplan.views.auth import login_user, register_user
 from nutriplan.views.categories import CategoryViewSet
@@ -21,4 +22,6 @@ urlpatterns = [
     path("", include(router.urls)),
     re_path(r"^auth/register/?$", register_user),
     re_path(r"^auth/login/?$", login_user),
+    re_path(r"^auth/refresh/?$", TokenRefreshView.as_view()),
+    re_path(r"^auth/verify/?$", TokenVerifyView.as_view()),
 ]
