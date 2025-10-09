03a527c54991b7a58922c064767c1c03450800cb
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-09 10:22:32 -0600

    nukear frontend

--- frontend/src/lib/components/search.ts
+++
@@ -1,6 +0,0 @@
-import { goto } from "$app/navigation";
-const search_path = "/search/";
-
-export function gotoSearch(search_term: string): void {
-  goto(search_path + search_term.replaceAll(' ', '-').toLowerCase());
-}
--- frontend/src/lib/endpoints.js
+++
@@ -1,4 +0,0 @@
-// Temporary for now
-export const API_ENDPOINT = "http://localhost:3000"
-
-export const AI_API_ENDPOINT = API_ENDPOINT + "/ai-endpoint"
