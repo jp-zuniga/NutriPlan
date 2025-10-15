f8247721f142bd9ee23ddba798079ab0512616e2
sea2horses <thefuzegamer06@gmail.com>
2025-10-15 15:31:12 -0600

    oops pt.2

--- frontend/src/lib/endpoints.js
+++ frontend/src/lib/endpoints.js
@@ -1,9 +1,6 @@
 import { PUBLIC_API_ENDPOINT } from '$env/static/public';
-import { NIGHTLY } from '$env/static/private';
 
 export const API_LOGIN_ENDPOINT = PUBLIC_API_ENDPOINT + '/auth/login/';
 export const API_REGISTER_ENDPOINT = PUBLIC_API_ENDPOINT + '/auth/register/';
 export const API_USERS_ENDPOINT = PUBLIC_API_ENDPOINT + '/users/';
 export const API_ME_ENDPOINT = PUBLIC_API_ENDPOINT + '/users/me/';
-
-export const NIGHTLY_BUILD = NIGHTLY == 'True';
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -21,11 +21,7 @@
 		rel="stylesheet"
 		href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
 	/>
-	{#if NIGHTLY_BUILD}
-		<title>NutriPlan</title>
-	{:else}
-		<title>NutriPlan (nightly)</title>
-	{/if}
+	<title>NutriPlan</title>
 </svelte:head>
 
 <Banner />
